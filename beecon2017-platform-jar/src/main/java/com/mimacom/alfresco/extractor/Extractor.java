cd75bc2bdf09a66f4661e89e8b3248562d366a19
sigi <simon.girardin@mimacom.com>
2017-04-25 12:45:45 +0200

    Changed Extractor interface signatures and added new method by regex

--- beecon2017-platform-jar/src/main/java/com/mimacom/alfresco/extractor/Extractor.java
+++ beecon2017-platform-jar/src/main/java/com/mimacom/alfresco/extractor/Extractor.java
@@ -4,6 +4,9 @@ package com.mimacom.alfresco.extractor;
 import org.alfresco.service.cmr.repository.NodeRef;
 import ucar.nc2.dataset.CoordinateAxis2D;
 
+import java.awt.*;
+import java.io.InputStream;
+
 /**
  * Interface for the extractor classes
  * Created by sigi on 25/04/2017.
@@ -11,5 +14,7 @@ import ucar.nc2.dataset.CoordinateAxis2D;
 public interface Extractor {
 
 
-    String extractMetaDateField(NodeRef node, CoordinateAxis2D top, CoordinateAxis2D bottom);
+    String extractMetaDataFieldByCoordinate(InputStream node, Point top, Point bottom);
+
+    String extractMetaDataFieldByRegex(InputStream node, String regex);
 }
